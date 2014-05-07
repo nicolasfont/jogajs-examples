9a6e94706777a970e297d2a5178c72ad16ce5d50
Nicolas Font <nicolasfont@gmail.com>
2014-05-07 15:50:06 -0700

    Add some html to humans app

--- humans/HumansApp.js
+++ humans/HumansApp.js
@@ -2,6 +2,7 @@ define(['joga', 'text!HumansApp.html', 'Human'], function (joga, html, Human) {
 
     function HumansApp() {
         this.element = joga.elementProperty(html);
+        this.humans = joga.arrayProperty([]);
     }
 
     return HumansApp;
