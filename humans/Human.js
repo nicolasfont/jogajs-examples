c5391406c7b7b1817ce61217034418610d06e60e
Nicolas Font <nicolasfont@gmail.com>
2014-06-09 05:42:33 +0000

    Add select and die

--- humans/Human.js
+++ humans/Human.js
@@ -1,10 +1,16 @@
 define(['joga', 'text!Human.html'], function (joga, html) {
 
-    function Human() {
-        this.name = joga.stringProperty("");
+    function Human(name) {
+        this.name = joga.stringProperty(name || "");
         this.isHappy = joga.booleanProperty(true);
         this.element = joga.elementProperty(html);
+        this.genders = joga.arrayProperty(['Male', 'Female', 'Other']);
+        this.gender = joga.stringProperty();
     }
+    
+    Human.prototype.die = function () {
+        this.element("<div>a dead human</div>");
+    };
 
     return Human;
 });
--- humans/HumansApp.js
+++ humans/HumansApp.js
@@ -4,6 +4,10 @@ define(['joga', 'text!HumansApp.html', 'Human'], function (joga, html, Human) {
         this.element = joga.elementProperty(html);
         this.humans = joga.arrayProperty([]);
     }
+    
+    HumansApp.prototype.addHuman = function () {
+        this.humans.push(new Human("a human"));
+    };
 
     return HumansApp;
 });
